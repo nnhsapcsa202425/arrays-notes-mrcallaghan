d4f6ed0a789316200eae7a5fcba526e019f7e9c4
mrcallaghan <mcallaghan@naperville203.org>
2024-12-12 09:18:19 -0600

    Added Chomp example code

--- CharMatrix.java
+++ CharMatrix.java
@@ -2,51 +2,57 @@
 
 public class CharMatrix
 {
-    // Instance variables:
+    // Fields:
+    private char[][] grid;
 
     // Constructor: creates a grid with dimensions rows, cols,
     // and fills it with spaces
     public CharMatrix(int rows, int cols)
     {
-
+        // call other constructor using 'this'
+        this(rows, cols, ' ');
     }
 
     // Constructor: creates a grid with dimensions rows , cols ,
     // and fills it with the fill  character
     public CharMatrix(int rows, int cols, char fill)
     {
+        this.grid = new char[rows][cols];
 
+        for(int row = 0; row < rows; row++)
+            for(int col = 0; col < cols; col++)
+                this.grid[row][col] = fill;
     }
 
     // Returns the number of rows in grid
     public int numRows()
     {
-        return 0;
+        return grid.length;
     }
 
     // Returns the number of columns in grid
     public int numCols()
     {
-        return 0;
+        return grid[0].length;
     }
 
     // Returns the character at row, col location
     public char charAt(int row, int col)
     {
-        return 'a';
+        return grid[row][col];
     }
 
     // Sets the character at row, col location to ch
     public void setCharAt(int row, int col, char ch)
     {
-
+        grid[row][col] = ch;
     }
 
     // Returns true if the character at row, col is a space,
     // false otherwise
     public boolean isEmpty(int row, int col)
     {
-        return false;
+        return (grid[row][col] == ' ');
     }
 
     // Fills the given rectangle with fill  characters.
@@ -54,6 +60,9 @@ public class CharMatrix
     // lower right corner of the rectangle.
     public void fillRect(int row0, int col0, int row1, int col1, char fill)
     {
+        for(int row = row0; row <= row1; row++)
+            for(int col = col0; col <= col1; col++)
+                grid[row][col] = fill;
 
     }
 
@@ -62,18 +71,30 @@ public class CharMatrix
     // lower right corner of the rectangle.
     public void clearRect(int row0, int col0, int row1, int col1)
     {
-
+        this.fillRect(row0, col0, row1, col1, ' ');
     }
 
     // Returns the count of all non-space characters in row 
     public int countInRow(int row)
     {
-        return 0;
+        int count = 0;
+
+        for(int col = 0; col < grid[row].length; col++)
+            if(grid[row][col] != ' ')
+                count++;
+
+        return count;
     }
 
     // Returns the count of all non-space characters in col 
     public int countInCol(int col)
     {
-        return 0;
+        int count = 0;
+
+        for(int row = 0; row < grid.length; row++)
+            if(grid[row][col] != ' ')
+                count++;
+
+        return count;
     }
 }
